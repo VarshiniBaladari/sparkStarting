e54f54200538d172fc90e9284ceb900939928133
varshini <varshini1101999@gmail.com>
2024-05-08 12:01:33 +0530

    log coded and configuration settings

--- realtime1.py
+++ realtime1.py
@@ -1,11 +1,16 @@
 from pyspark.sql import *
+from pyspark import SparkConf
 from lib.logger import log4J
 
 if __name__ == "__main__":
+    conf = SparkConf()
+    conf.set("spark.app.name", "Hello Spark")
+    conf.set("spark.master","local[3]")
     spark = SparkSession.builder \
-        .appName("Realtime1") \
-        .master("local[4]") \
+        .config(conf=conf)\
         .getOrCreate()
     logger = log4J(spark)
+    conf_out = spark.sparkContext.getConf()
     logger.info("starting realtime example")
+    logger.info(conf_out.toDebugString())
     logger.info("Finished realtime spark example")
