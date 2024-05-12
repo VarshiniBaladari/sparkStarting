f7cece1830ee3ee8daeeb7e1ce619a477d4595c8
varshini <varshini1101999@gmail.com>
2024-05-13 00:49:50 +0530

    many more changes

--- StartingWithFunctions.py
+++ StartingWithFunctions.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from data import *
 
 # Import the global variable from the first script
 from second import global_df
@@ -8,6 +9,11 @@ if __name__ == "__main__":
         .appName("Hello Spark") \
         .master("local[4]") \
         .getOrCreate()
+    cleaned_bnb = "data\\Cleaned_airbnb_barcelona.csv"
+    df2 = "data\\barcelona_listings.csv"
+    airbnb_cleaned = spark.read.csv(cleaned_bnb, header=True, inferSchema=True)
+    listings = spark.read.csv(df2, header=True, inferSchema=True)
 
     # Now you can use global_df in your second script
-    global_df.show()
+    listings.show()
+
--- realtime1.py
+++ realtime1.py
@@ -1,15 +1,17 @@
+import sys
 from pyspark.sql import *
 from pyspark import SparkConf
 from lib.logger import log4J
 
 if __name__ == "__main__":
     conf = SparkConf()
-    conf.set("spark.app.name", "Hello Spark")
+    conf.set("spark.app.name", "Realtime1")
     conf.set("spark.master","local[3]")
     spark = SparkSession.builder \
         .config(conf=conf)\
         .getOrCreate()
     logger = log4J(spark)
+    
     conf_out = spark.sparkContext.getConf()
     logger.info("starting realtime example")
     logger.info(conf_out.toDebugString())
